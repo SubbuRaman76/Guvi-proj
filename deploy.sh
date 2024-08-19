f8312dcf27aefb199ef2663711dd98daaafcc74a
Manikandan16497 <manikandan16497@gmail.com>
2024-08-19 13:06:34 +0000

    add all files

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-docker login -u amanikandan16497 -p dckr_pat_2S56HcG2uOxhyOd2UbVrqwAe_vg
+
 if [ $GIT_BRANCH = "dev" ]; then
-    # Build your project
-    sh 'chmod +x build.sh'
-    sh './build.sh'
-    docker tag nginximage amanikandan16497/dev
-    docker push amanikandan16497/dev
-
-elif [ $GIT_BRANCH = "master" ]; then
-    sh 'chmod +x build.sh'
-    sh './build.sh'
-    docker tag nginximage amanikandan16497/prod 
-    docker push amanikandan16497/prod
-fi
-
+   
