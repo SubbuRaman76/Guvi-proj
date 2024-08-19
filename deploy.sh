7fc8d45238792af0eb5f8ae722fcefea27fef5dd
Manikandan16497 <157342078+Manikandan16497@users.noreply.github.com>
2024-08-19 20:33:41 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 
 if [ "$GIT_BRANCH" = "dev" ]; then
     # Build your project
-    sh 'chmod +x build.sh'
-    sh './build.sh'
+    'chmod +x build.sh'
+    './build.sh'
     docker tag nginximage amanikandan16497/dev
     docker push amanikandan16497/dev
 
 elif [ "$GIT_BRANCH" = "master" ]; then
-    sh 'chmod +x build.sh'
-    sh './build.sh'
+    'chmod +x build.sh'
+    './build.sh'
     docker tag nginximage amanikandan16497/prod 
     docker push amanikandan16497/prod
 fi
